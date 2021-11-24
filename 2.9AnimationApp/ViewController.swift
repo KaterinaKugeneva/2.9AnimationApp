dec204d5cb3d06f20d337f42ae0d021a7fdbf8d0
KaterinaKugeneva <kugeneva1@gmail.com>
2021-11-24 10:21:21 +0300

    Initial Commit

---
+++ 2.9AnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.9AnimationApp
+//
+//  Created by Ekaterina Kugeneva on 24.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
